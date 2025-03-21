a6a20f66d39d86d17c04f25d24a1065a607442ee
wycliff <wyckieochieng91@gmail.com>
2025-03-21 10:49:52 +0300

    typo

--- test.py
+++ test.py
@@ -61,7 +61,7 @@ if __name__ == "__main__":
     app.add_handler(CommandHandler('help', help))
     app.add_handler(CommandHandler('custom', custom))
 
-    app.add_handler(MessageHandler(filters.TEXT, handle_message))
+    app.add_handler(MessageHandler(filter.TEXT, handle_message))
 
     app.add_error_handler(error)
 
