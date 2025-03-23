589d07bdc5e34b474cb17258d1b1113f64a49568
wycliff <wyckieochieng91@gmail.com>
2025-03-23 23:45:24 +0300

    first

--- michezo_mart.py
+++ michezo_mart.py
@@ -1,3 +1,11 @@
 import telegram
 from telegram import Update
 from telegram.ext import ContextTypes,ApplicationBuilder,MessageHandler,CommandHandler
+
+#commands
+async def start(update,context):
+    pass
+async def help(update,context):
+    pass
+async def custom(update,context):
+    pass
